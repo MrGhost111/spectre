fa3b24918134d2cfea077eccca856c5975422bcb
MrGhost111 <108256659+MrGhost111@users.noreply.github.com>
2024-08-19 15:41:26 -0400

    Update snipe.js

---
+++ commands/snipe.js
@@ -0,0 +1,20 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+
+let lastDeletedMessage = null;
+
+client.on('messageDelete', message => {
+    lastDeletedMessage = message;
+});
+
+client.on('messageCreate', message => {
+    if (message.content === '!snipe') {
+        if (lastDeletedMessage) {
+            message.channel.send(`Last deleted message: ${lastDeletedMessage.content}`);
+        } else {
+            message.channel.send('No messages have been deleted recently.');
+        }
+    }
+});
+
+client.login(process.env.DISCORD_TOKEN);
