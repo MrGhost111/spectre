2a7c601e5f02f9781cd670566531a50efd338a84
MrGhost111 <108256659+MrGhost111@users.noreply.github.com>
2024-08-20 14:22:10 -0400

    Update esnipe.js

--- commands/esnipe.js
+++ commands/esnipe.js
@@ -1,17 +1,9 @@
-const { client } = require('./index.js');
-
-let lastEditedMessage = null;
-
-client.on('messageUpdate', (oldMessage, newMessage) => {
-    lastEditedMessage = { oldMessage, newMessage };
-});
-
-client.on('messageCreate', message => {
-    if (message.content === ',esnipe') {
-        if (lastEditedMessage) {
-            message.channel.send(`Last edited message: ${lastEditedMessage.oldMessage.content} -> ${lastEditedMessage.newMessage.content}`);
-        } else {
-            message.channel.send('No messages have been edited recently.');
-        }
-    }
-});
+module.exports = {
+  name: 'esnipe',
+  execute(message) {
+    const editedMessages = message.channel.messages.cache.filter(m => m.editedTimestamp);
+    const latestEdited = editedMessages.sort((a, b) => b.editedTimestamp - a.editedTimestamp).first();
+    if (!latestEdited) return message.channel.send('No edited messages found.');
+    message.channel.send({ embeds: [{ title: 'Edited Message', description: `**Before:** ${latestEdited.content}, \n**After:** ${latestEdited.content}`, timestamp: latestEdited.editedTimestamp }] });
+  }
+};
