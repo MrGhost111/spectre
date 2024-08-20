5a227b124c3dbf8df65533d7fa54fceb29097d0d
MrGhost111 <108256659+MrGhost111@users.noreply.github.com>
2024-08-20 14:21:52 -0400

    Update snipe.js

--- commands/snipe.js
+++ commands/snipe.js
@@ -1,15 +1,9 @@
-// snipe.js
-const { SlashCommandBuilder } = require('discord.js');
-
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('snipe')
-    .setDescription('Snipes the last deleted message'),
-  async execute(interaction, client) {
-    // Implement your sniping logic here
-    //  access the channel using interaction.channel
-    //  ....
-    await interaction.reply('Your sniped message');
-  },
+  name: 'snipe',
+  execute(message) {
+    const deletedMessages = message.channel.messages.cache.filter(m => m.deleted);
+    const latestDeleted = deletedMessages.sort((a, b) => b.createdTimestamp - a.createdTimestamp).first();
+    if (!latestDeleted) return message.channel.send('No deleted messages found.');
+    message.channel.send({ embeds: [{ title: 'Sniped Message', description: latestDeleted.content, timestamp: latestDeleted.createdAt }] });
+  }
 };
-
