017ce1e1113e3995b02a2e8701527a7fcd0eb49a
MrGhost111 <108256659+MrGhost111@users.noreply.github.com>
2025-03-08 15:08:13 -0500

    test

--- text-commands/stfu.js
+++ text-commands/stfu.js
@@ -431,7 +431,8 @@ module.exports = {
                     resultMessage + '\n\n' +
                     `<:YJ_streak:1259258046924853421> Streak: ${streakDisplay}\n` +
                     `<:idk:1064831073881694278> Luck: **${totalLuck}**`
-                )
+            )
+                .setFooter({ text: `Command used by: ${message.author.id}` })
                 .setImage('https://media.discordapp.net/attachments/986130247692996628/1259196768822759444/battlefield-2042-ezgif.com-crop.gif?ex=66f64020&is=66f4eea0&hm=6422c352520ce212a6144066b0ded88fa4cd68bc02b15c41beb3d81612616ef1&=&width=750&height=251')
                 ;
 
