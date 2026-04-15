8218a0684b361300bea79b810dd06658c23758e2
MrGhost111 <bhargavshiroya123@gmail.com>
2026-04-15 03:54:00 -0400

    b

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-// index.js
+//
+//index.js
 const { Client, GatewayIntentBits, Collection, ActivityType, Partials } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
