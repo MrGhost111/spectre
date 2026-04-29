f3d6bab24498afdb908a05ccaebde50e6d263625
MrGhost111 <bhargavshiroya123@gmail.com>
2026-04-28 23:55:29 -0400

    abc

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -51,7 +51,7 @@ async function buildLeaderboard(sorted, page, totalPages, interaction, event) {
         const totalFmt = formatFull(total);
         const youTag = isYou ? '  <:sweg:1010054002202906634>' : '';
 
-        description += `${dot} [${displayName}](${RICKROLL}) — ${currency} ${totalFmt}${youTag}\n`;
+        description += `${dot} [${currency} ${totalFmt} - ${displayName}](${RICKROLL}) ${youTag}\n`;
     }
 
     return new EmbedBuilder()
